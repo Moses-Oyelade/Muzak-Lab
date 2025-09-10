4dbe96f667845a720376c3a38d69b09acd51388b
Moses-Oyelade <oyelademoses@gmail.com>
2025-09-11 00:01:37 +0100

    set current username on auth

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -13,9 +13,11 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
         token['role'] = user.role
+        token['username'] = user.username
         return token
 
     def validate(self, attrs):
         data = super().validate(attrs)
         data['role'] = self.user.role
+        data['username'] = self.user.username
         return data
