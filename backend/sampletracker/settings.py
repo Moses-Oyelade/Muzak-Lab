94843b89514482d5f847305326b782e5cd6c8d33
moses_oyelade <oyelademoses@gmail.com>
2025-11-09 20:16:16 -0330

    settings update

--- backend/sampletracker/settings.py
+++ backend/sampletracker/settings.py
@@ -85,32 +85,23 @@ WSGI_APPLICATION = 'sampletracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('POSTGRES_DB', 'muzaklab'),
-#         'USER': os.environ.get('POSTGRES_DB', 'myuser'),
-#         'PASSWORD': os.environ.get('POSTGRES_DB', ''),
-#         'HOST': os.environ.get('POSTGRES_DB', 'localhost'),
-#         'PORT': os.environ.get('POSTGRES_DB', '5432'),
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST', 'localhost'),
+#         'PORT': os.getenv('DB_PORT', '5432'),
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
-}
 
 
 # Password validation
