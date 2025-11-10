4d8e804b3ae535aef09ebde206c3af6d3a6fb17e
moses_oyelade <oyelademoses@gmail.com>
2025-11-09 23:00:48 -0330

    settings update

--- backend/sampletracker/settings.py
+++ backend/sampletracker/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-+=!rypm82++dy6(2_9h8ya6=02=inb7ine3bk057voy72z6%$e'
-)
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
